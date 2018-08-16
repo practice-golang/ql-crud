5b7a298bb68ebfb852ad1eae832e44d4e4a55f0f
Practice Golang <practice.golang@gmail.com>
2018-08-16 18:09:22 +0900

    working...

--- src/ql-crud/ql-crud.go
+++ src/ql-crud/ql-crud.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/cznic/ql"
 )
@@ -23,7 +24,37 @@ func insertData(db *ql.DB) error {
 		`begin transaction;
 		insert into books (id, title, author) values (1, "Book Title", "Author Man");
 		insert into books (id, title, author) values (2, "Titleaaa", "Girl");
-		insert into books (title, author) values ("Booooooooooook", "Childddd");
+		commit;`)
+	if err != nil {
+		return err
+	}
+
+	a, _, err := db.Run(ql.NewRWCtx(),
+		`begin transaction;
+		select id() from books order by id() desc limit 1;
+		commit;`)
+	if err != nil {
+		return err
+	}
+	lastData, _ := a[0].FirstRow()
+	lastID := uint64(lastData[0].(int64) + 1)
+
+	_, _, err = db.Run(ql.NewRWCtx(),
+		`begin transaction;
+		insert into books values (`+strconv.FormatUint(lastID, 10)+`, "Booooooooooook", "Childddd");
+		commit;`)
+	if err != nil {
+		fmt.Println("WTF::", err)
+		return err
+	}
+
+	return nil
+}
+
+func updateData(db *ql.DB) error {
+	_, _, err := db.Run(ql.NewRWCtx(),
+		`begin transaction;
+		update books set title="Titan^_^_^", author="Woman" where id==2;
 		commit;`)
 	if err != nil {
 		return err
@@ -35,14 +66,14 @@ func insertData(db *ql.DB) error {
 func selectData(db *ql.DB) error {
 	a, b, err := db.Run(ql.NewRWCtx(),
 		`begin transaction;
-		select * from books order by id asc;
+		select id(), id, title, author from books order by id() desc;
 		commit;`)
 	if err != nil {
 		return err
 	}
 
 	fmt.Println(a)
-	data, _ := a[0].Rows(99, 0)
+	data, _ := a[0].Rows(999, 0)
 	fmt.Println(data)
 	fmt.Println(b)
 
@@ -73,6 +104,9 @@ func main() {
 		panic(err)
 	}
 	if err = insertData(db); err != nil {
+		panic(err)
+	}
+	if err = updateData(db); err != nil {
 		panic(err)
 	}
 	if err = selectData(db); err != nil {
