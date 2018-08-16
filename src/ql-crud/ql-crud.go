771ea4fa860c28cf165010255751b13d5d4c5fae
Practice Golang <practice.golang@gmail.com>
2018-08-16 17:07:09 +0900

    working

--- src/ql-crud/ql-crud.go
+++ src/ql-crud/ql-crud.go
@@ -1,76 +1,84 @@
 package main
 
 import (
-	"crypto/sha1"
-	"database/sql"
 	"fmt"
-	"io"
-	"log"
-	"os"
-	"path/filepath"
 
 	"github.com/cznic/ql"
 )
 
-func setUp(db *sql.DB) error {
-	tx, err := db.Begin()
+func createTable(db *ql.DB) error {
+	_, _, err := db.Run(ql.NewRWCtx(),
+		`begin transaction;
+		create table if not exists books (id bigint, title string, author string);
+		commit;`)
 	if err != nil {
 		return err
 	}
-	_, err = tx.Exec(`create table books (id bigint , title string, body string, created_at string, updated_at string);`)
+
+	return nil
+}
+
+func insertData(db *ql.DB) error {
+	_, _, err := db.Run(ql.NewRWCtx(),
+		`begin transaction;
+		insert into books (id, title, author) values (1, "Book Title", "Author Man");
+		insert into books (id, title, author) values (2, "Titleaaa", "Girl");
+		insert into books (title, author) values ("Booooooooooook", "Childddd");
+		commit;`)
 	if err != nil {
 		return err
 	}
-	if err = tx.Commit(); err != nil {
+
+	return nil
+}
+
+func selectData(db *ql.DB) error {
+	a, b, err := db.Run(ql.NewRWCtx(),
+		`begin transaction;
+		select * from books order by id asc;
+		commit;`)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(a)
+	data, _ := a[0].Rows(99, 0)
+	fmt.Println(data)
+	fmt.Println(b)
+
+	return nil
+}
+
+func dropTable(db *ql.DB) error {
+	_, _, err := db.Run(ql.NewRWCtx(),
+		`begin transaction;
+		drop table books;
+		commit;`)
+	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func tearDown(db *sql.DB) error {
-	tx, err := db.Begin()
-	if err != nil {
-		return err
-	}
-	_, err = tx.Exec(`drop table note;`)
-	if err != nil {
-		return err
-	}
-	if err = tx.Commit(); err != nil {
-		return err
-	}
-	return nil
-}
-
-const dbname = "./ql.db"
-
-func walName(dbname string) (r string) {
-	base := filepath.Base(filepath.Clean(dbname))
-	h := sha1.New()
-	io.WriteString(h, base)
-
-	return filepath.Join(filepath.Dir(dbname), fmt.Sprintf(".%x", h.Sum(nil)))
-}
+const dbname = "ql.db"
 
 func main() {
-	wName := walName(dbname)
-
-	ql.RegisterDriver()
-
-	db, err := sql.Open("ql", dbname)
-	defer os.Remove(wName)
+	db, err := ql.OpenFile(dbname, &ql.Options{CanCreate: true, RemoveEmptyWAL: true})
 	defer db.Close()
 	if err != nil {
-		log.Fatalf("failed to open db: %s", err)
+		panic(err)
 	}
 
-	if err = setUp(db); err != nil {
-		log.Fatalf("failed to create table: %s", err)
+	if err = createTable(db); err != nil {
+		panic(err)
 	}
-	if err = db.Ping(); err != nil {
-		log.Fatalf("failed to ping db: %s", err)
+	if err = insertData(db); err != nil {
+		panic(err)
 	}
-	if err = tearDown(db); err != nil {
-		log.Fatalf("failed to drop table: %s", err)
+	if err = selectData(db); err != nil {
+		panic(err)
+	}
+	if err = dropTable(db); err != nil {
+		panic(err)
 	}
 }
