28a9784e18533d2e702e10bb077f8e1ae9f99730
Practice Golang <practice.golang@gmail.com>
2018-08-16 21:23:26 +0900

    대충하자 -_- 이건 여기까지

--- src/ql-crud/ql-crud.go
+++ src/ql-crud/ql-crud.go
@@ -29,14 +29,14 @@ func insertData(db *ql.DB) error {
 		return err
 	}
 
-	a, _, err := db.Run(ql.NewRWCtx(),
+	s, _, err := db.Run(ql.NewRWCtx(),
 		`begin transaction;
 		select id() from books order by id() desc limit 1;
 		commit;`)
 	if err != nil {
 		return err
 	}
-	lastData, _ := a[0].FirstRow()
+	lastData, _ := s[0].FirstRow()
 	lastID := uint64(lastData[0].(int64) + 1)
 
 	_, _, err = db.Run(ql.NewRWCtx(),
@@ -54,7 +54,7 @@ func insertData(db *ql.DB) error {
 func updateData(db *ql.DB) error {
 	_, _, err := db.Run(ql.NewRWCtx(),
 		`begin transaction;
-		update books set title="Titan^_^_^", author="Woman" where id==2;
+		update books set title="Bokkk name^_^_^", author="Woman" where id==2;
 		commit;`)
 	if err != nil {
 		return err
@@ -64,7 +64,7 @@ func updateData(db *ql.DB) error {
 }
 
 func selectData(db *ql.DB) error {
-	a, b, err := db.Run(ql.NewRWCtx(),
+	s, _, err := db.Run(ql.NewRWCtx(),
 		`begin transaction;
 		select id(), id, title, author from books order by id() desc;
 		commit;`)
@@ -72,10 +72,8 @@ func selectData(db *ql.DB) error {
 		return err
 	}
 
-	fmt.Println(a)
-	data, _ := a[0].Rows(999, 0)
+	data, _ := s[0].Rows(99, 0)
 	fmt.Println(data)
-	fmt.Println(b)
 
 	return nil
 }
@@ -88,10 +86,11 @@ func dropTable(db *ql.DB) error {
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
 
-const dbname = "ql.db"
+const dbname = "./ql.db"
 
 func main() {
 	db, err := ql.OpenFile(dbname, &ql.Options{CanCreate: true, RemoveEmptyWAL: true})
@@ -103,15 +102,18 @@ func main() {
 	if err = createTable(db); err != nil {
 		panic(err)
 	}
+
 	if err = insertData(db); err != nil {
 		panic(err)
 	}
 	if err = updateData(db); err != nil {
 		panic(err)
 	}
+
 	if err = selectData(db); err != nil {
 		panic(err)
 	}
+
 	if err = dropTable(db); err != nil {
 		panic(err)
 	}
